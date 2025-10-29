e1a6760606b329b11f4679158fde730eb8eb1882
Jonas Thiago <146122741+jonas-thiago@users.noreply.github.com>
2025-10-29 15:57:26 -0300

    version10
    barra e footer feito

---
+++ include_navbar.js
@@ -0,0 +1,13 @@
+// O 'pathToRoot' será definido em cada página HTML
+fetch(pathToRoot + '/navbar.html')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Erro ao carregar o navbar. Verifique o caminho.');
+    }
+    return response.text();
+  })
+  .then(data => {
+    // Certifique-se de que você tem um <div id="navbar-placeholder"></div> no seu HTML
+    document.querySelector('#navbar-placeholder').innerHTML = data;
+  })
+  .catch(error => console.error('Erro no include_navbar.js:', error));
